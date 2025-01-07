f798b6d859c4a1ea53f4f16de5f5e5ee089cf78c
Zayne Peladas <123078039+BeauBeannie@users.noreply.github.com>
2025-01-07 14:55:04 +0800

    Add on/off background music (only in mainmenu)

--- Assets/Scripts/ToggleSwitch.cs
+++ Assets/Scripts/ToggleSwitch.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class ToggleSwitch : MonoBehaviour {
     public GameObject switchOn, switchOff;
+    private bool muted = false;
 
     public void OnChangeValue() {
         bool onoffSwitch = gameObject.GetComponent<Toggle>().isOn;
@@ -20,5 +21,50 @@ public class ToggleSwitch : MonoBehaviour {
 
         }
     }
+
+    void Start()
+    {
+        if(!PlayerPrefs.HasKey("muted"))
+        {
+            PlayerPrefs.SetInt("muted", 0);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+
+        AudioListener.pause = muted;
+    }
+
+    public void OnButtonPress()
+    {
+        if(muted == false)
+        {
+            muted = true;
+            AudioListener.pause = true;
+        }
+        else
+        {
+            muted = false;
+            AudioListener.pause = false;
+        }
+
+        Save();
+    }
+
+    //this is for the player's pref (PlayerPref)
+
+    private void Load()
+    {
+        muted = PlayerPrefs.GetInt("muted") == 1;
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetInt("muted", muted ? 1 : 0);
+    }
+
+
 }
 
