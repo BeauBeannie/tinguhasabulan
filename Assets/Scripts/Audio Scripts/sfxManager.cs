bcdfc14c613d89608dbb3fb2a78fca347db6e3b4
Zayne Peladas <123078039+BeauBeannie@users.noreply.github.com>
2025-01-22 00:01:28 +0800

    Add sfx on button clicks (chapter and page flips) and background music in journal

--- Assets/Scripts/Audio Scripts/sfxManager.cs
+++ Assets/Scripts/Audio Scripts/sfxManager.cs
@@ -7,6 +7,8 @@ public class sfxManager : MonoBehaviour
     public static sfxManager instance { get; private set; }
 
     [SerializeField] private AudioSource buttonClick;
+    [SerializeField] private AudioSource chapterFlip;
+    [SerializeField] private AudioSource pageFlip;
 
     private void Awake()
     {
@@ -31,4 +33,30 @@ public class sfxManager : MonoBehaviour
             Debug.LogWarning("No AudioSource assigned to buttonClick in sfxManager.");
         }
     }
+
+    public void PlayChapterFlipClick()
+    {
+        if (chapterFlip != null)
+        {
+            chapterFlip.Play();
+        }
+        else
+        {
+            Debug.LogWarning("No AudioSource assigned to chapterFlip in sfxManager.");
+        }
+    }
+
+    public void PlayPageFlipClick()
+    {
+        if (pageFlip != null)
+        {
+            pageFlip.Play();
+        }
+        else
+        {
+            Debug.LogWarning("No AudioSource assigned to pageFlip in sfxManager.");
+        }
+    }
+    
+    
 }
