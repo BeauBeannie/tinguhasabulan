05e0c6f3fceba1d02fd377baa5cf82df91e956f4
Zayne Peladas <123078039+BeauBeannie@users.noreply.github.com>
2025-01-25 01:19:38 +0800

    Create up down movement slider for the dragon

--- Assets/Scripts/HDragonController.cs
+++ Assets/Scripts/HDragonController.cs
@@ -1,37 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HDragonController : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField] private float speed; // Horizontal and forward/backward speed
+    [SerializeField] private float verticalSpeed; // Vertical movement speed
+    [SerializeField] private Slider verticalSlider; // Reference to the vertical slider
 
     private FixedJoystick fixedJoystick;
     private Rigidbody rigidBody;
 
+    private bool isDraggingSlider = false; // Tracks whether the slider is being held
+
     private void OnEnable()
     {
+        // Initialize joystick and Rigidbody
         fixedJoystick = FindObjectOfType<FixedJoystick>();
-        rigidBody = gameObject.GetComponent<Rigidbody>();
+        rigidBody = GetComponent<Rigidbody>();
+
+        verticalSlider = GameObject.Find("Slider").GetComponent<Slider>();
+
+
+        // Set slider range and default value
+        verticalSlider.minValue = -1f;
+        verticalSlider.maxValue = 1f;
+        verticalSlider.value = 0f;
+
+        // Add listener for value changes
+        verticalSlider.onValueChanged.AddListener(OnSliderValueChanged);
     }
 
     private void FixedUpdate()
     {
+        // Handle horizontal movement with joystick
         float xVal = fixedJoystick.Horizontal;
         float yVal = fixedJoystick.Vertical;
 
-        Debug.Log($"Joystick Input - Horizontal: {xVal}, Vertical: {yVal}"); // Log joystick input
-
-
         Vector3 movement = new Vector3(xVal, 0, yVal);
-        rigidBody.velocity = movement * speed;
+        rigidBody.velocity = new Vector3(movement.x * speed, rigidBody.velocity.y, movement.z * speed);
 
-
-        if(xVal != 0 && yVal != 0)
+        // Rotate the dragon to face movement direction
+        if (xVal != 0 || yVal != 0)
         {
-            transform.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.Atan2(xVal, yVal)*Mathf.Rad2Deg, transform.eulerAngles.z);
+            transform.eulerAngles = new Vector3(
+                transform.eulerAngles.x,
+                Mathf.Atan2(xVal, yVal) * Mathf.Rad2Deg,
+                transform.eulerAngles.z
+            );
+        }
 
-            Debug.Log($"Joystick Input - Horizontal: {xVal}, Vertical: {yVal}"); // Log joystick input
+        // Reset slider value to 0 smoothly when not dragging
+        if (!isDraggingSlider && Mathf.Abs(verticalSlider.value) > 0.01f)
+        {
+            verticalSlider.value = Mathf.Lerp(verticalSlider.value, 0f, Time.deltaTime * 5f);
+        }
+    }
+
+    // Called when the slider value changes (while being dragged)
+    private void OnSliderValueChanged(float value)
+    {
+        isDraggingSlider = true; // Slider is being interacted with
+        rigidBody.velocity = new Vector3(rigidBody.velocity.x, value * verticalSpeed, rigidBody.velocity.z);
+    }
+
+    private void LateUpdate()
+    {
+        // Detect when the slider interaction is released
+        if (Input.GetMouseButtonUp(0) || Input.touchCount == 0)
+        {
+            isDraggingSlider = false; // User has released the slider
         }
     }
 }
