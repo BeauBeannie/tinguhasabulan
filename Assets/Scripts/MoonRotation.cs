35c9af4f4d3b149ac9034b4fc8e9a850054a8bb2
Zayne Peladas <123078039+BeauBeannie@users.noreply.github.com>
2025-02-05 22:53:15 +0800

    Fix the moon counter by adding a box collider on the dragon

--- Assets/Scripts/MoonRotation.cs
+++ Assets/Scripts/MoonRotation.cs
@@ -9,6 +9,7 @@ public class MoonRotation : MonoBehaviour
     void Update()
     {
         transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.down * rotationSpeed * Time.deltaTime);
     }
 
 
