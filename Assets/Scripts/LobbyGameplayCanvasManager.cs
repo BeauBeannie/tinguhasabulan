be36107017376323b55fece3971f18bb2e21fd32
Zayne Peladas <123078039+BeauBeannie@users.noreply.github.com>
2025-03-12 01:20:32 +0800

    Modify tutorial board merge in each level scenes and replace music&sfx

--- Assets/Scripts/LobbyGameplayCanvasManager.cs
+++ Assets/Scripts/LobbyGameplayCanvasManager.cs
@@ -7,6 +7,8 @@ public class LobbyGameplayCanvasManager : MonoBehaviour
     [SerializeField] private GameObject lobbyCanvas;
     [SerializeField] private GameObject gameplayCanvas;
     [SerializeField] private GameObject inGamePauseCanvas;
+    [SerializeField] private GameObject tutorialBoard;
+
 
 
     public void Awake()
@@ -18,15 +20,20 @@ public class LobbyGameplayCanvasManager : MonoBehaviour
         }
 
         //Enable the gameplay canvas
-        if(gameplayCanvas != null)
+        if (gameplayCanvas != null)
         {
             gameplayCanvas.SetActive(false);
         }
 
         //Disable In-Game Menu Canvas
-        if(inGamePauseCanvas != null)
+        if (inGamePauseCanvas != null)
         {
             inGamePauseCanvas.SetActive(false);
+        }
+
+        if (tutorialBoard != null)
+        {
+            tutorialBoard.SetActive(false);
         }
     }
 
@@ -40,15 +47,57 @@ public class LobbyGameplayCanvasManager : MonoBehaviour
         }
 
         //Enable the gameplay canvas
-        if(gameplayCanvas != null)
+        if (gameplayCanvas != null)
         {
             gameplayCanvas.SetActive(true);
         }
 
         //Disable In-Game Menu Canvas
-        if(inGamePauseCanvas != null)
+        if (inGamePauseCanvas != null)
         {
             inGamePauseCanvas.SetActive(false);
+        }
+
+        if (tutorialBoard != null)
+        {
+            tutorialBoard.SetActive(false);
+        }
+    }
+
+    public void ShowTutorial()
+    {
+        // Show tutorial board, hide others
+        if (lobbyCanvas != null)
+        {
+            lobbyCanvas.SetActive(false);
+        }
+
+        if (gameplayCanvas != null)
+        {
+            gameplayCanvas.SetActive(false);
+        }
+
+        if (inGamePauseCanvas != null)
+        {
+            inGamePauseCanvas.SetActive(false);
+        }
+
+        if (tutorialBoard != null)
+        {
+            tutorialBoard.SetActive(true);
+        }
+    }
+
+    public void CloseTutorial()
+    {
+        if (tutorialBoard != null)
+        {
+            tutorialBoard.SetActive(false);
+        }
+
+        if (lobbyCanvas != null)
+        {
+            lobbyCanvas.SetActive(true);
         }
     }
 }
